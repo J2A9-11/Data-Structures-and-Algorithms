581c1e45e9f49370c3e78419a2de1770be280993
J2A9-11 <jaianand1129@gmail.com>
2024-01-26 20:25:37 +0530

    linkedlist

--- linkedlist.java
+++ linkedlist.java
@@ -52,6 +52,7 @@ public class linkedlist {
             tail.data=data;
             tail.next=null;
         }
+        display();
         
     }
 
@@ -74,6 +75,7 @@ public class linkedlist {
             head=ne;
             head.data=data;
         }
+        display();
     }
 
 
@@ -88,6 +90,7 @@ public class linkedlist {
             head=ne;
             head.data=da;
             tail=head;
+            display();
         }
         else
         {
@@ -102,6 +105,7 @@ public class linkedlist {
             tail=ne;
             tail.data=da;
             tail.next=temp;
+            display();
             }
 
 
@@ -123,6 +127,7 @@ public class linkedlist {
             tail=ne;
             tail.data=data;
             tail.next=temp;
+            display();
         }
         else
         {
@@ -150,6 +155,7 @@ public class linkedlist {
                 tail.next=null;
 
             }
+            display();
         }
         else 
         System.out.println("Position not found");
@@ -161,7 +167,7 @@ public class linkedlist {
     // Adding a data at the given position in a given linked list
     
 
-    public void atIndex(int index, int data)
+    public void atIndex(int index, int data,int n)
     {
         Node ne= new Node();
         if (index==1)
@@ -169,19 +175,18 @@ public class linkedlist {
             ne.next=head;
             head=ne;
             head.data=data;
+            display();
         }
         else
         {
         tail=head;
-        if(index>=2)
+        if(index>=2 && index<=n+1)
         {        
             while(index-->2 && tail!=null)
             {
                 tail=tail.next;
             }
-            if(tail==null)
-            System.out.println("Position not found");
-            else if (tail.next!=null)
+            if (tail.next!=null)
             {
                 Node temp=tail.next;
                 tail.next=ne;
@@ -195,6 +200,7 @@ public class linkedlist {
                 tail.data=data;
                 tail.next=null;
             }
+            display();
         }
         else
         System.out.println("Position not found");
@@ -218,8 +224,10 @@ public class linkedlist {
             tail=tail.next;pos=pos+1;}
             if(tail.next==null && tail.data!=da)
             System.out.println("Data " +da+" is not present in linked list ");
-            else 
+            else{ 
             System.out.println("Data "+da+ " found at position "+pos+" in linked list");
+            display();
+            }
         }
     }
 
@@ -270,6 +278,104 @@ public class linkedlist {
     }
 
 
+    // Deleting the node after the given data in a linked list
+
+
+    public void delafter(int da)
+    {
+        if(head==null)
+        display();
+        else{
+            tail=head;
+            while(tail.data!=da && tail.next!=null)
+            {
+                tail=tail.next;
+            }
+            if(tail.data!=da && tail.next==null)
+            System.out.println("Data not found");
+            else if(tail.data==da && tail.next==null)
+            System.out.println("This is the last node ");
+            else{
+                Node temp=tail.next;
+                tail.next=temp.next;
+                temp=null;
+                display();
+            }
+        }
+    }
+
+
+    // Deleting the node after the given position in a linked list
+
+
+    public void delposition(int pos)
+    {
+        if(head==null)
+        display();
+        else{
+            if(pos>=0)
+            {
+                tail=head;
+                if(pos==0)
+                {
+                    head=head.next;
+                    display();
+                }
+                else
+                {
+                    while(pos--!=1 && tail!=null)
+                    tail=tail.next;
+                    if(tail==null)
+                    System.out.println("Position not found");
+                    else if(tail.next==null)
+                    System.out.println("This is the last node of linked list");
+                    else
+                    {
+                        Node temp=tail.next;
+                        tail.next=temp.next;
+                        temp=null;
+                        display();
+                    }
+                }
+            }
+            else
+            System.out.println("Position not found");
+        }
+    }
+
+
+    // Deleting a node at the given position in a linked list
+
+
+    public void delatposition(int pos,int n)
+    {
+        if (head==null)
+        display();
+        else{
+            if(pos>=1 && pos<=n)
+            {
+                tail=head;
+                if(pos==1)
+                {
+                    head=head.next;
+                    display();
+                }
+                else
+                {
+                    while(pos--!=2 && tail!=null){
+                    tail=tail.next;}                  
+                    Node temp=tail.next;
+                    tail.next=temp.next;
+                    display();
+                }
+            }
+            else
+            System.out.println("Position not found");
+        }
+    }
+
+
+
     // Displaying a linked list
 
 
@@ -282,7 +388,7 @@ public class linkedlist {
         {
             System.out.print(tail.data+"\t");
             tail=tail.next;
-        }System.out.println();
+        }System.out.println("\n");
     }
 
     public static void main(String[] args) {
@@ -290,6 +396,7 @@ public class linkedlist {
         System.out.println("Enter number of nodes in a linked list-");
         int n=sc.nextInt();
         linkedlist obj=new linkedlist();
+        if(n>0){
         System.out.println("Enter elements of linked list");
         for(int i=0;i<n;i++)
         {
@@ -300,32 +407,38 @@ public class linkedlist {
 
         System.out.println("Adding element at the last of a linked list-");
         obj.addLast(80);
-        obj.display();
 
         System.out.println("Adding element at the beginning of a linked list-");
         obj.addBegin(100);
-        obj.display();
 
         System.out.println("Adding element after the given data in a linked list-");
         obj.inbetween(456, 50);
-        obj.display();
         
         System.out.println("Adding element after the given position in a linked list-");
         obj.index(0,109);
-        obj.display();
 
         System.out.println("Adding element at the given index in a linked list-");
-        obj.atIndex(2,16);
-        obj.display();
+        obj.atIndex(4,16,n);
 
         System.out.println("Searching the data in linked list-");
         obj.search(50);
-        obj.display();
 
         System.out.println("Deleting the last node from a linked list-");
         obj.delLast();
 
         System.out.println("Deleting the first node from a linked list-");
         obj.delFirst();
+
+        System.out.println("Deleting the node from a linked list after a given data-");
+        obj.delafter(30);
+
+        System.out.println("Deleting the node from a linked list after a given position-");
+        obj.delposition(0);
+
+        System.out.println("Deleting the node from a linked list at a given position-");
+        obj.delatposition(5,n);
+        }
+        else 
+        obj.display();
     }
 }
